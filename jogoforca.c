d032dcc0dd2e3d0737ce25ca3fb42a62c4867c04
brunaolv01 <143206621+brunaolv01@users.noreply.github.com>
2023-09-15 13:43:41 -0300

    Update jogoforca.c

--- jogoforca.c
+++ jogoforca.c
@@ -4,87 +4,153 @@
 #include <locale.h>
 #include <time.h>
 
+#define MAX_WORD_LENGTH 256
+#define MAX_GUESSES 7
+
+void forca(erros){
+	
+    printf(" -------------\n");
+    printf(" |           |\n");
+    printf(" |           %s\n", ((erros >= 1)? "O" : " "));
+    printf(" |          %s%s%s\n", ((erros >= 3)? "/" : " "), ((erros >= 2)? "|" : " "), ((erros >= 4)? "\\" : " "));
+    printf(" |           %s\n", ((erros >= 5)? "|" : " "));
+    printf(" |           %s%s\n", ((erros >= 6)? "/" : " "), ((erros >= 7)? "\\" : " "));
+    printf("---           \n\n\n");
+	
+}
+
 void displayHeader() {
     printf("*******************************************\n");
-    printf("******* BEM VINDO AO JOGO DA FORCA!********\n");
+    printf("******* BEM VINDO AO JOGO DA FORCA! ******\n");
     printf("*******************************************\n\n\n\n");
 }
 
-void clearScreen() {
-    system("clear");
-}
-
-void addWordToDatabase(){
+void addWordToDatabase() {
     displayHeader();
     FILE* file;
-    int i;
-    file = fopen("palavrasescondidas.txt", "a");
-    char word[256];
-    fflush(stdin);
-    printf("Qual palavra você deseja adicionar?\n\n\nDigite aqui sua resposta  ");
+    char word[MAX_WORD_LENGTH];
+    
+    printf("Qual palavra você deseja adicionar?\n\n\nDigite aqui sua resposta: ");
     scanf("%s", word);
-    fprintf(file, "\n%s", word);
+    
+    file = fopen("palavrasescondidas.txt", "a");
+    
+	if (file == NULL) {
+    	printf("Erro ao abrir o arquivo.\n");
+        return;
+    }
+    
+    fprintf(file, "%s\n", word);
     fclose(file);
 }
 
-void deleteWordFromDatabase(){
+void deleteWordFromDatabase() {
     displayHeader();
     FILE* file;
-    int i, lineCount = 0, deleteChoice;
+    int lineCount = 0, deleteChoice;
+    
     file = fopen("palavrasescondidas.txt", "r");
-    char buffer[256], word[256];
-    while (fgets(buffer, sizeof(buffer), file)) { 
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return;
+    }
+    
+    char word[MAX_WORD_LENGTH];
+    while (fgets(word, sizeof(word), file)) { 
         lineCount++;
     }
     fclose(file);
+    
+    if (lineCount == 0) {
+        printf("Banco de palavras vazio.\n");
+        return;
+    }
+    
     printf("Qual palavra deseja apagar?\n\n\n");
     file = fopen("palavrasescondidas.txt", "r");
+    
+    int i;
+    
     for (i = 0; i < lineCount; i++) {
-        fscanf(file, "\n%s", word);
-        printf("%d) %s\n", i+1, word);
+        fscanf(file, "%s", word);
+        printf("%d) %s\n", i + 1, word);
     }
     fclose(file);
-    printf("\nDigite sua resposta aqui:");
+    
+    printf("\nDigite sua resposta aqui: ");
     scanf("%d", &deleteChoice);
-    file = fopen("palavrasescondidas.txt","r");
-    FILE* tempFile = fopen("palavrasescondidas.txt", "w");
+    
+    if (deleteChoice < 1 || deleteChoice > lineCount) {
+        printf("Escolha inválida.\n");
+        return;
+    }
+    
+    file = fopen("palavrasescondidas.txt", "r");
+    FILE* tempFile = fopen("temp.txt", "w");
+    
     for (i = 0; i < lineCount; i++) {
         fgets(word, sizeof(word), file);
         if (i != deleteChoice - 1) {
             fprintf(tempFile, "%s", word);
         }
     }
+    
     fclose(file);
     fclose(tempFile);
     remove("palavrasescondidas.txt");
-    rename("palavrasescondidas.txt", "palavrasescondidas.txt");
+    rename("temp.txt", "palavrasescondidas.txt");
 }
 
 void modifyWordInDatabase() {
     displayHeader();
     FILE* file;
-    int i, lineCount = 0, modifyChoice;
+    int lineCount = 0, modifyChoice;
+    int i;
+    
     file = fopen("palavrasescondidas.txt", "r");
-    char buffer[256], word[256], newWord[256];
-    while (fgets(buffer, sizeof(buffer), file)) { 
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return;
+    }
+    
+    char word[MAX_WORD_LENGTH], newWord[MAX_WORD_LENGTH];
+    
+    while (fgets(word, sizeof(word), file)) { 
         lineCount++;
     }
     fclose(file);
+    
+    if (lineCount == 0) {
+        printf("Banco de palavras vazio.\n");
+        return;
+    }
+    
     printf("Qual palavra deseja alterar?\n\n\n");
     file = fopen("palavrasescondidas.txt", "r");
+    
     for (i = 0; i < lineCount; i++) {
-        fscanf(file, "\n%s", word);
-        printf("%d) %s\n", i+1, word);
+        fscanf(file, "%s", word);
+        printf("%d) %s\n", i + 1, word);
     }
     fclose(file);
-    printf("\nDigite aqui sua resposta:");
+    
+    printf("\nDigite aqui sua resposta: ");
     scanf("%d", &modifyChoice);
-    clearScreen();
+    
+    if (modifyChoice < 1 || modifyChoice > lineCount) {
+        printf("Escolha inválida.\n");
+        return;
+    }
+    
+    system("cls");
     displayHeader();
-    printf("Por qual palavra deseja substituir?\n\n\nDigite aqui  ");
+    
+    printf("Por qual palavra deseja substituir?\n\n\nDigite aqui: ");
     scanf("%s", newWord);
+    
     file = fopen("palavrasescondidas.txt", "r");
-    FILE* tempFile = fopen("palavrasescondidas.txt", "w");
+    FILE* tempFile = fopen("temp.txt", "w");
+    
     for (i = 0; i < lineCount; i++) {
         fgets(word, sizeof(word), file);
         if (i == modifyChoice - 1) {
@@ -93,166 +159,161 @@ void modifyWordInDatabase() {
             fprintf(tempFile, "%s", word);
         }
     }
+    
     fclose(file);
     fclose(tempFile);
     remove("palavrasescondidas.txt");
-    rename("palavrasescondidas.txt", "palavrasescondidas.txt");
+    rename("temp.txt", "palavrasescondidas.txt");
+}
+
+int chooseRandomWord(char *word) {
+    FILE* file;
+    int lineCount = 0;
+    int i;
+    
+    file = fopen("palavrasescondidas.txt", "r");
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return 0;
+    }
+    
+    char line[MAX_WORD_LENGTH];
+    
+    while (fgets(line, sizeof(line), file)) { 
+        lineCount++;
+    }
+    
+    if (lineCount == 0) {
+        printf("Banco de palavras vazio.\n");
+        fclose(file);
+        return 0;
+    }
+    
+    srand(time(NULL));
+    int randomLineNumber = rand() % lineCount + 1;
+    
+    fseek(file, 0, SEEK_SET);
+    
+    for (i = 0; i < randomLineNumber; i++) {
+        fgets(line, sizeof(line), file);
+    }
+    
+
+    strtok(line, "\n");
+    
+    fclose(file);
+    
+    strcpy(word, line);
+    
+    return 1;
 }
 
 void playHangman() {
-    FILE* file;
+    char wordToGuess[MAX_WORD_LENGTH];
+    char guessedWord[MAX_WORD_LENGTH];
+    int numGuesses = 0;
+    int wordLength;
     int i;
-    file = fopen("palavrasescondidas.txt", "r");
-    int lineCount = 0, letterIndex = 0, letterCount = 0, correctGuesses = 0, errorCounter = 0, errors = 0, chosenLettersCount = 0, bodyCounter = 0, bodyCounter2 = 0, repeatedLetter = 0, guessedCorrectly = 0;
-    char buffer[256], word[256], letter[256], guessedLetter, correctLetters[256], chosenLetters[26], incorrectLetters[256];
-    while (fgets(buffer, sizeof(buffer), file)) { 
-        lineCount++;
-    }
-    fclose(file);
-    srand(time(NULL));
-    int randomWordIndex = rand();
-    int scannedWordIndex = randomWordIndex % lineCount;
-    scannedWordIndex++;
-    file = fopen("palavrasescondidas.txt", "r");
-    for(i = 0; i < scannedWordIndex; i++){
-        fscanf(file, "%s", word);
-    }
-    fclose(file);
-    for (i = 0; word[i] != '\0'; i++) {
-        if (isalpha(word[i])) {
-            letterCount++;
-        }
-            }
-    for (i = 0; word[i] != '\0'; i++) {
-        if (isalpha(word[i])) {
-            letter[letterIndex] = word[i];
-            letterIndex++;
-        }
-    }
-    printf("Palavra selecionada: %s\n", word);
-    clearScreen();
-    for (i = 0; i < letterCount; i++) {
-        correctLetters[i] = '_';
-    }
-    for (i = 0; i < 7; i++) {
-        incorrectLetters[i] = '_';
-    }
-    correctLetters[letterCount] = '\0';
-    incorrectLetters[letterCount] = '\0';
+    int letterGuessed = 0;
+    char guess;
+    int erros = 0;
     
-    do {
-        if(bodyCounter2 == 0){
-            displayHeader();
-            printf(" -------------\n");
-            printf(" |           |\n");
-            printf(" |            \n");
-            printf(" |            \n");
-            printf(" |            \n");
-            printf(" |            \n");
-            printf("---           \n");
+    if (!chooseRandomWord(wordToGuess)) {
+        printf("Não há palavras no banco de palavras. Adicione palavras para jogar.\n");
+        return;
+    }
+    
+    wordLength = strlen(wordToGuess);
+    
+    for (i = 0; i < wordLength; i++) {
+        guessedWord[i] = '_';
+    }
+    
+    guessedWord[wordLength] = '\0';
+    
+    while (1) {
+        system("cls");
+        displayHeader();
+        forca(erros);
+        
+        printf("Palavra a ser adivinhada: %s\n", guessedWord);
+        printf("Tentativas restantes:");
+        printf("%d/%d\n", numGuesses, MAX_GUESSES);
+        
+        if (numGuesses == MAX_GUESSES) {
+            printf("Você excedeu o número máximo de tentativas. A palavra era: %s\n", wordToGuess);
+            break;
         }
         
-        fflush(stdin);
-        printf("\nDigite sua resposta aqui ===>  ");
-        scanf(" %c", &guessedLetter);
+        printf("Adivinhe uma letra: ");
+        scanf(" %c", &guess);
         
-        for (i = 0; i < chosenLettersCount; i++) {
-            if (tolower(guessedLetter) == chosenLetters[i]) {
-                repeatedLetter = 1;
+        for (i = 0; i < wordLength; i++) {
+            if (guess == guessedWord[i]) {
+                printf("Você já adivinhou essa letra.\n");
+                letterGuessed = 1;
+                
                 break;
             }
         }
         
-        if (!repeatedLetter) {
-            chosenLetters[chosenLettersCount] = tolower(guessedLetter);
-            chosenLettersCount++;
-            
-            for (i = 0; i < letterCount; i++) {
-                if (tolower(guessedLetter) == tolower(letter[i])) {
-                    correctLetters[i] = letter[i];
-                    guessedCorrectly = 1;
-                    correctGuesses++;
+        if (!letterGuessed) {
+            letterGuessed = 0;
+            for (i = 0; i < wordLength; i++) {
+                if (guess == wordToGuess[i]) {
+                    guessedWord[i] = guess;
+                    letterGuessed = 1;
                 }
             }
             
-            if (!guessedCorrectly) {
-                incorrectLetters[errorCounter++] = guessedLetter;
-                errors++;
-                bodyCounter++;
+            if (!strcmp(wordToGuess, guessedWord)) {
+                system("cls");
+                displayHeader();
+                printf("Parabéns! Você adivinhou a palavra: %s\n", wordToGuess);
+                break;
             }
+            if (!letterGuessed) {
+                printf("Letra incorreta. Tente novamente.\n");
+                numGuesses++;
+                erros++;               
+            }
+        } else {
+            letterGuessed = 0;
         }
-        
-        clearScreen();
-        displayHeader();
-        
-        printf(" -------------\n");
-        printf(" |           |\n");
-        printf(" |           %s\n", ((bodyCounter >= 1)? "O" : " "));
-        printf(" |          %s%s%s\n", ((bodyCounter >= 3)? "/" : " "), ((bodyCounter >= 2)? "|" : " "), ((bodyCounter >= 4)? "\\" : " "));
-        printf(" |           %s\n", ((bodyCounter >= 5)? "|" : " "));
-        printf(" |           %s%s\n", ((bodyCounter >= 6)? "/" : " "), ((bodyCounter >= 7)? "\\" : " "));
-        printf("---           \n\n\n");
-        
-        bodyCounter2++;
-        printf("Letras que errou: %s\n\n", incorrectLetters);
-        printf("%s\n", correctLetters);
-        
-    } while (errors <= 7 && correctGuesses < letterCount);
+    }
 }
 
 int main() {
     setlocale(LC_ALL, "portuguese");
-    clearScreen();
+    system("cls");
     displayHeader();
-    int menu;
-    int choice;
+    int menuChoice;
+    
     do {
-        printf("1) Jogar\n2) Alterar Palavra\n3) Excluir Palavra\n4) Adicionar Palavra\n5) Sair\n\n\nDigite sua resposta aqui ====> ");
-        scanf("%d", &menu);
-        clearScreen();
-        switch(menu) {
+        printf("1) Jogar\n2) Alterar Palavra\n3) Excluir Palavra\n4) Adicionar Palavra\n5) Sair\n\n\nDigite sua resposta aqui: ");
+        scanf("%d", &menuChoice);
+        system("cls");
+        
+        switch (menuChoice) {
             case 1:
                 playHangman();
-                printf("Deseja voltar para o menu principal?\n1) Sim\n2) Não\n\n\nDigite sua resposta aqui ====> ");
-                scanf("%d", &choice);
-                if (choice == 1) {
-                    main();
-                }
                 break;
             case 2:
                 modifyWordInDatabase();
-                clearScreen();
-                displayHeader();
-                printf("Deseja voltar para o menu principal?\n1) Sim\n2) Não\n\n\nDigite sua resposta aqui ====> ");
-                scanf("%d", &choice);
-                if (choice == 1) {
-                    main();
-                }
                 break;
             case 3:
                 deleteWordFromDatabase();
-                clearScreen();
-                displayHeader();
-                printf("Deseja voltar para o menu principal?\n1) Sim\n2) Não\n\n\nDigite sua resposta aqui ====> ");
-                scanf("%d", &choice);
-                if (choice == 1) {
-                    main();
-                }
                 break;
             case 4:
                 addWordToDatabase();
-                clearScreen();
-                displayHeader();
-                printf("Deseja voltar para o menu principal?\n1) Sim\n2) Não\n\n\nDigite sua resposta aqui ====> ");
-                scanf("%d", &choice);
-                if (choice == 1) {
-                    main();
-                }
                 break;
+            case 5:
+                printf("Obrigado por jogar! Adeus.\n");
+                break;
+            default:
+                printf("Opção inválida.\n");
         }
-    } while ((choice < 1) || (choice > 5));
-    
+    } while (menuChoice != 5);
 }
-
         
 
